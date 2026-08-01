3f8308588450b6e046856e270663eaab8f2fbb79
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-08-01 22:34:06 +0530

    Time: 0 ms (100%), Space: 10.4 MB (10.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,19 @@
 class Solution {
 private:
-    long long game(vector<int>& nums, int i , int j){
+    long long game(vector<int>& nums, int i , int j,vector<vector<int>> & dp){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        long long taken_i = nums[i]+ min(game(nums,i+1,j-1),game(nums,i+2,j));
-        long long taken_j = nums[j] + min(game(nums,i+1,j-1),game(nums,i,j-2));
-        return max(taken_i, taken_j);
-    }
+        if(dp[i][j]!=-1) return dp[i][j];
+        long long taken_i = nums[i]+ min(game(nums,i+1,j-1,dp),game(nums,i+2,j,dp));
+        long long taken_j = nums[j] + min(game(nums,i+1,j-1,dp),game(nums,i,j-2,dp));
+        return dp[i][j] = max(taken_i, taken_j);
+    } 
 public:
     bool predictTheWinner(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
         long long sum = 0;
         for(auto i : nums) sum+=i;
-        long long player_1 = game(nums,0,nums.size()-1);
+        long long player_1 = game(nums,0,nums.size()-1,dp);
         return player_1 >= sum-player_1;
     }
 };
