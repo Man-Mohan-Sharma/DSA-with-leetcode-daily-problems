98138c1d3e3c44d88580bddc782b646582cb99de
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-01 12:23:53 +0530

    Time: 0 ms (100%), Space: 10 MB (97.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,0);
-        dp[1] = nums[0];
+        int first = 0, second = nums[0], thrid = 0;
         for(int i = 1; i<nums.size(); i++){
-            dp[i+1] = max(nums[i]+dp[i-1],dp[i]);
+            thrid = max(nums[i]+first,second);
+            first = second;
+            second = thrid;
         }
-        return dp[nums.size()];
+        return second;
     }
 };
