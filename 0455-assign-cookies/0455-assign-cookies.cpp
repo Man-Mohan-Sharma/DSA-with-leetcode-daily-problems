d928ca63697a8553c23e9a33902b8ddfa6aaf70e
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-04 10:47:02 +0530

    Time: 8 ms (59.33%), Space: 44.7 MB (97.38%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,16 +3,15 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int i = g.size()-1, j = s.size()-1;
-        int ans = 0;
-        while(i>=0 && j>=0){
+        int i = 0, j = 0 , ans = 0;
+        while(i<g.size() && j<s.size()){
             if(g[i]<=s[j]){
                 ans++;
-                i--;
-                j--;
+                i++;
+                j++;
             }
-            else i--;
+            else j++;
         }
         return ans;
-    }
+        }
 };
