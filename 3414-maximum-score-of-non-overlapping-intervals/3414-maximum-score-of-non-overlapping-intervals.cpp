866856307403294206bb9d498de6775ab75738eb
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-12 15:37:04 +0530

    Time: 2752 ms (5.43%), Space: 352.1 MB (31.52%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,121 +1,57 @@
 class Solution {
-    struct Node {
+    struct Node{
         vector<int> arr;
-        long long score = LLONG_MIN;
+        long long score = INT_MIN;
     };
 
 private:
-    int findNext(vector<vector<int>>& intervals,
-                 int low,
-                 int high,
-                 int target) {
-
+    int find(vector<vector<int>>& intervals, int low, int high, int target){
         int ans = intervals.size();
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (intervals[mid][0] > target) {
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(intervals[mid][0]>target){
                 ans = mid;
-                high = mid - 1;
+                high = mid-1;
             }
-            else {
-                low = mid + 1;
-            }
+            else low = mid+1;
         }
-
         return ans;
     }
 
-    Node solve(vector<vector<int>>& intervals,
-               int i,
-               int k,
-               vector<int>& nextterm,
-               vector<vector<Node>>& dp) {
-
-        if (i >= intervals.size() || k == 4) {
+    Node solve(vector<vector<int>>& intervals, int i, int k,vector<int>& nextterm,vector<vector<Node>>& dp){
+        if(i >= intervals.size() || k == 4) {
             Node base;
             base.score = 0;
             return base;
         }
-
-        if (dp[i][k].score != LLONG_MIN)
-            return dp[i][k];
-
-        // Don't take
-        Node skip = solve(
-            intervals,
-            i + 1,
-            k,
-            nextterm,
-            dp
-        );
-
-        // Take
-        Node taken = solve(
-            intervals,
-            nextterm[i],
-            k + 1,
-            nextterm,
-            dp
-        );
-
-        int originalIndex = intervals[i][3];
-        long long weight = intervals[i][2];
-
-        taken.score += weight;
-        taken.arr.push_back(originalIndex);
-
-        // Important for lexicographical comparison
-        sort(taken.arr.begin(), taken.arr.end());
-
+        if(dp[i][k].score!=INT_MIN) return dp[i][k];
+        Node skip = solve(intervals,i+1,k,nextterm,dp);
+        Node taken = solve(intervals,nextterm[i],k+1,nextterm,dp);
+        int idx = intervals[i][3];
+        long long score = intervals[i][2];
+        taken.arr.push_back(idx);
+        taken.score+=score;
+        sort(taken.arr.begin(),taken.arr.end());
         Node result;
-
-        if (taken.score > skip.score) {
-            result = taken;
+        if(skip.score>taken.score) result = skip;
+        else if(taken.score>skip.score) result = taken;
+        else{
+            result = (skip.arr < taken.arr)?skip:taken;
         }
-        else if (skip.score > taken.score) {
-            result = skip;
-        }
-        else {
-            // vector comparison is lexicographical
-            result = (taken.arr < skip.arr) ? taken : skip;
-        }
-
-        return dp[i][k] = result;
+        return  dp[i][k] = result;
     }
 
 public:
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
-
+        for(int i = 0; i<intervals.size(); i++) intervals[i].push_back(i);
         int n = intervals.size();
-
-        // Store original index
-        for (int i = 0; i < n; i++) {
-            intervals[i].push_back(i);
-        }
-
-        // Sort according to starting point
-        sort(intervals.begin(), intervals.end());
-
+        vector<vector<Node>> dp(n,vector<Node>(4,Node()));
+        sort(intervals.begin(),intervals.end());
         vector<int> nextterm(n);
-
-        for (int i = 0; i < n; i++) {
-            nextterm[i] = findNext(
-                intervals,
-                i + 1,
-                n - 1,
-                intervals[i][1]
-            );
+        for(int i = 0; i<n; i++){
+            nextterm[i] = find(intervals,i+1,n-1,intervals[i][1]);
         }
-
-        vector<vector<Node>> dp(
-            n,
-            vector<Node>(4)
-        );
-
         Node result = solve(intervals,0,0,nextterm,dp);
-
         return result.arr;
     }
 };
