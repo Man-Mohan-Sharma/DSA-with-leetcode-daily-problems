612d21818b4fd4fe7041c90ff12231a7be4c6ff0
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-15 10:59:24 +0530

    Time: 543 ms (12.82%), Space: 216.8 MB (13.87%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,25 +1,17 @@
 class Solution {
 private:
-    bool is_plindrome(string& s, int i, int j){
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    int solve(string& s, int k , int i, int j,vector<vector<int>>& dp){
+    int solve(string& s, int k , int i, int j,vector<vector<int>>& dp,vector<vector<bool>>& is_palindrome){
         if(i>=s.size() || j>=s.size()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(is_plindrome(s,i,j)){
-            int take = 1+solve(s,k,j+1,j+k,dp);
-            int grow = solve(s,k,i,j+1,dp);
-            int slide = solve(s,k,i+1,j+1,dp);
+        if(is_palindrome[i][j]){
+            int take = 1+solve(s,k,j+1,j+k,dp,is_palindrome);
+            int grow = solve(s,k,i,j+1,dp,is_palindrome);
+            int slide = solve(s,k,i+1,j+1,dp,is_palindrome);
             return dp[i][j] = max(take,max(grow,slide));
         }
         else{
-            int grow = solve(s,k,i,j+1,dp);
-            int slide = solve(s,k,i+1,j+1,dp);
+            int grow = solve(s,k,i,j+1,dp,is_palindrome);
+            int slide = solve(s,k,i+1,j+1,dp,is_palindrome);
             return dp[i][j] = max(grow,slide);
         }
     }
@@ -27,7 +19,16 @@ private:
 public:
     int maxPalindromes(string s, int k) {
         if(k==1) return s.size();
+        vector<vector<bool>> is_palindrome(s.size()+1,vector<bool>(s.size()+1,false));
+        for(int l = 1; l<=s.size(); l++){
+            for(int i = 0; i+l<=s.size(); i++){
+                int j = i+l-1;
+                if(i==j) is_palindrome[i][j] = true;
+                else if(i+1 == j) is_palindrome[i][j] = (s[i]==s[j])?true:false;
+                else is_palindrome[i][j] = (s[i]==s[j] && is_palindrome[i+1][j-1])?true:false;
+            }
+        }
         vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,-1));
-        return solve(s,k,0,k-1,dp);
+        return solve(s,k,0,k-1,dp,is_palindrome);
     }
 };
