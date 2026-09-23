2279c76afeb75fd2b35452bdb185da1c6b277645
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-23 12:31:38 +0530

    Time: 14 ms (8.26%), Space: 32.3 MB (6.56%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int count = 0;
-        for(int i = 0; i<arr.size(); i++){
-            for(int j = i+1; j<arr.size(); j++){
-                for(int k = j+1; k<arr.size(); k++){
-                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c) count++;
-                }
+        vector<int> freq(1001,0);
+        int ans = 0;
+        for(int j = 1; j<arr.size(); j++){
+            freq[arr[j-1]]++;
+            vector<int> prefix(1001,0);
+            prefix[0] = freq[0];
+            for(int i = 1; i<1001; i++) prefix[i] = prefix[i-1]+freq[i];
+            for(int k = j+1; k<arr.size(); k++){
+                if(abs(arr[j]-arr[k])>b) continue;
+                int l = max(0,max(arr[j]-a,arr[k]-c));
+                int r = min(1000,min(arr[j]+a,arr[k]+c));
+                if(l>r) continue;
+                ans+= prefix[r];
+                if(l>0) ans-=prefix[l-1];
             }
         }
-        return count;
+        return ans;
     }
 };
