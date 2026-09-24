2a311ca7167799e94e08af9ba49f167a49957ed6
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-24 19:39:30 +0530

    Time: 12 ms (19.37%), Space: 14.2 MB (50.13%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        vector<int> ans(nums.size());
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-        for(int i = 0; i < nums.size(); i++){
-            pq.push({nums[i], i});
-        }
-
-        int count = 0, pre = -1, freq = 0;
-
-        while(!pq.empty()){
-            auto [node, idx] = pq.top();
-
-            if(pre != node){
-                count += freq;
-                pre = node;
-                freq = 1;
+        vector<int> ans;
+        for(int i = 0; i<nums.size(); i++){
+            int count = 0;
+            for(int j = 0; j<nums.size(); j++){
+                if(i==j) continue;
+                if(nums[i]>nums[j]) count++;
             }
-            else{
-                freq++;
-            }
-
-            ans[idx] = count;
-
-            pq.pop();
+            ans.push_back(count);
         }
-
         return ans;
     }
 };
