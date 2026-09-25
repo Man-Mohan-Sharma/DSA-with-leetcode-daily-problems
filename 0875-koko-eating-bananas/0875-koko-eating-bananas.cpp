87436de8ae82a72f8ad31d60c35880ff8507d472
Man-Mohan-Sharma <manmohan311206@gmail.com>
2026-09-25 23:10:50 +0530

    Time: 28 ms (5.01%), Space: 22.9 MB (58.39%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    long long find(vector<int>& piles, int k){
+        long long  total = 0;
+        for(auto& i : piles){
+            total+= ceil(double(i)/double(k));
+        }
+        return total;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(),piles.end());
+        int low = 1, high = piles[piles.size()-1];
+        int ans = high;
+        while(low<=high){
+            long long mid = low+(high-low)/2;
+            if(find(piles,mid)<=h){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+};
